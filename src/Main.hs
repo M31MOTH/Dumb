39e851813beb34e12de54551e6d7b9eb79a3eae4
Giovani Ferreira <giovani.silva@nubank.com.br>
2018-02-02 07:41:19 -0200

    Working, arg parsing needs to improve

--- src/Main.hs
+++ src/Main.hs
@@ -11,6 +11,7 @@ import Data.ByteString (ByteString)
 import Data.ByteString.Char8 (pack, unpack)
 --import Data.ByteString.Char8 (pack)
 import System.Environment (getArgs)
+import Data.Either.Unwrap (whenRight)
 
 getWordlist :: FilePath -> IO [String]
 getWordlist filename = do
@@ -23,20 +24,25 @@ generateSubdomains :: String -> [String] -> [ByteString]
 generateSubdomains domain wordlist =
   map pack $ map (++ "." ++ domain) wordlist
 
---resolve :: Domain -> IO (Either DNSError [Data.IP.Addr.IPv4])
+getIPs :: Show a => [a] -> String
+getIPs [x] = (show x)
+getIPs (x:xs) = getIPs xs ++ "|" ++ (show x)
+
+printAll :: Show a => ByteString -> [a] -> IO ()
+printAll sub [] = return ()
+printAll sub lst = putStrLn $ "[+] Found subdomain " ++ (unpack sub) ++ " <=> " ++ (getIPs lst)
+
 resolve :: Domain -> IO ()
 resolve subdomain = do
   rs <- makeResolvSeed defaultResolvConf
   result <- withResolver rs $ \resolver -> lookupA resolver subdomain
-  case result of
-    Left err -> print ("Could not find subdomain " ++ (unpack subdomain))
-    Right ip -> print ("[+] Found subdomain " ++ (unpack subdomain) ++ " <=> "++ (show (head ip)))
+  whenRight (result) (printAll subdomain)
 
 main :: IO ()
 main = do
-  let domain = "nubank.com.br"
-      filename = "/tmp/teste"
+  args <- getArgs
+  let domain = head args
+      filename = args !! 1
   wordlist <- getWordlist filename
   mapM resolve $ generateSubdomains domain wordlist
-  --mapM_ putStrLn result
   putStrLn $ "Hello World"
